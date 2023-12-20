babf7f05bf08e99fd00c7349910f44b062bfe12a
Yuji Arima <arimayuji2@gmail.com>
2023-12-20 16:05:26 -0300

    get user metrics unity tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -19,6 +19,17 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
 		return checkIn;
 	}
+	
+	async countByUserId(userId: string) {
+		return this.items.filter((checkIn) => checkIn.user_id == userId).length;
+	}
+
+	async findManyByUserId(userId: string, page: number) {
+		return this.items
+			.filter((checkIn) => checkIn.user_id == userId)
+			.slice((page - 1) * 20, page * 20);
+	}
+
 	async findByUserIdOnDate(userId: string, date: Date) {
 		const startOfTheDay = dayjs(date).startOf("date");
 		const endOfTheDay = dayjs(date).endOf("date");
