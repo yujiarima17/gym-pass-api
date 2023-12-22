655e9f195471882cb16f19c9a898214616d118e1
Yuji Arima <arimayuji2@gmail.com>
2023-12-22 11:37:30 -0300

    feat:get user profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
+import { verifyJWT } from "./middlewares/verifiy-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", register);
+
 	app.post("/sessions", authenticate);
+
+	// rotas apenas chamadas quando o usuário estiver autenticado
+	app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
